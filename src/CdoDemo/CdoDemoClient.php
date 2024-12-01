9a883c6c3ce3990bda5496e826a64d3ab2b03830
Arthur Falque Pierrotin <arthur.fp.dev@pm.me>
2024-12-01 21:43:56 +0100

    :sparkles: Basic client service to fetch from demo API + basic controller to test it

---
+++ src/CdoDemo/CdoDemoClient.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\CdoDemo;
+
+use App\Exception\CdoDemoException;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpClient\HttpOptions;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class CdoDemoClient
+{
+    public function __construct(
+        private HttpClientInterface $httpClient,
+
+        #[Autowire(env: 'CDO_DEMO_USERNAME')]
+        private readonly string $cdoDemoUsername,
+
+        #[Autowire(env: 'CDO_DEMO_PASSWORD')]
+        private readonly string $cdoDemoPassword,
+    ) {
+        $this->httpClient = $httpClient->withOptions(
+            (new HttpOptions())
+                ->setBaseUri("https://api-demo.cdo.fr/api/")
+                ->toArray()
+        );
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws DecodingExceptionInterface
+     */
+    public function getToken(): string
+    {
+        $response = $this->httpClient->request(
+            'POST',
+            'login_check',
+            [
+                'json' => [
+                    'username' => $this->cdoDemoUsername,
+                    'password' => $this->cdoDemoPassword,
+                ]
+            ]
+        );
+
+        $content = $response->toArray();
+
+        if (!array_key_exists('token', $content)) {
+            throw new CdoDemoException(message: "Key 'token' not found in CDO Demo API response.");
+        }
+
+        return $content['token'];
+    }
+}
---
+++ src/Controller/UploadController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\CdoDemo\CdoDemoClient;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+
+class UploadController extends AbstractController
+{
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    #[Route('/upload', name: 'upload')]
+    public function upload(CdoDemoClient $cdoDemoClient): Response
+    {
+        return $this->render(
+            'upload/index.html.twig',
+            [
+                'token' => $cdoDemoClient->getToken()
+            ]
+        );
+    }
+}
